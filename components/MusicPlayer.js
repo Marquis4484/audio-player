ea53142dcbb7bf15ae080238afe0cb7a6d7ad07d
Marquis Sampson <marquis4484@gmail.com>
2022-05-08 19:25:15 -0400

    initial commit

---
+++ components/MusicPlayer.js
@@ -0,0 +1,94 @@
+import React, { useState, useRef, useEffect } from 'react'
+import styles from "../styles/MusicPlayer.module.css";
+import { BsArrowLeftShort } from "react-icons/bs"
+import { BsArrowRightShort } from "react-icons/bs"
+import { FaPlay } from "react-icons/fa"
+import { FaPause } from "react-icons/fa"
+
+const MusicPlayer = () => {
+  // state
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  // references
+  const musicPlayer = useRef();   // reference our audio component
+  const progressBar = useRef();   // reference our progress bar
+  const animationRef = useRef();  // reference the animation
+
+  useEffect(() => {
+    const seconds = Math.floor(musicPlayer.current.duration);
+    setDuration(seconds);
+    progressBar.current.max = seconds;
+  }, [musicPlayer?.current?.loadedmetadata, musicPlayer?.current?.readyState]);
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes}:${returnedSeconds}`;
+  }
+
+  const togglePlayPause = () => {
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      musicPlayer.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying)
+    } else {
+      musicPlayer.current.pause();
+      cancelAnimationFrame(animationRef.current);
+    }
+  }
+
+  const whilePlaying = () => {
+    progressBar.current.value = musicPlayer.current.currentTime;
+    changePlayerCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  }
+
+  const changeRange = () => {
+    musicPlayer.current.currentTime = progressBar.current.value;
+    changePlayerCurrentTime();
+  }
+
+  const changePlayerCurrentTime = () => {
+    progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`)
+    setCurrentTime(progressBar.current.value);
+  }
+
+  const backThirty = () => {
+    progressBar.current.value = Number(progressBar.current.value - 30);
+    changeRange();
+  }
+
+  const forwardThirty = () => {
+    progressBar.current.value = Number(progressBar.current.value + 30);
+    changeRange();
+  }
+
+  return (
+    <div className={styles.musicPlayer}>
+      <audio ref={musicPlayer} src="../../public/music/bensound-jazzyfrenchy.mp3" preload="metadata"></audio>
+      <button className={styles.forwardBackward} onClick={backThirty}><BsArrowLeftShort /> 30</button>
+      <button onClick={togglePlayPause} className={styles.playPause}>
+        {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
+      </button>
+      <button className={styles.forwardBackward} onClick={forwardThirty}>30 <BsArrowRightShort /></button>
+
+      {/* current time */}
+      <div className={styles.currentTime}>{calculateTime(currentTime)}</div>
+
+      {/* progress bar */}
+      <div>
+        <input type="range" className={styles.progressBar} defaultValue="0" ref={progressBar} onChange={changeRange} />
+      </div>
+
+      {/* duration */}
+      <div className={styles.duration}>{(duration && !isNaN(duration)) && calculateTime(duration)}</div>
+    </div>
+  )
+}
+
+export { MusicPlayer }
